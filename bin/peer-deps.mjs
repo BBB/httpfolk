4c33e829d18eeb45875e700b13ca55f1c18e3bdb
Ollie Relph <ollie@relph.me>
2023-01-15 17:09:21 +0000

    Script for peer deps and some setup config

---
+++ bin/peer-deps.mjs
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import fs from "node:fs"
+import packageDef from "../package.json" assert {type: "json"};
+
+const peers = {
+    "@fp-ts/schema": "^0.0.8"
+}
+
+packageDef.peerDependencies = peers
+packageDef.devDependencies = {
+    ...packageDef.devDependencies,
+    ...peers
+}
+
+fs.writeFileSync("package.json", JSON.stringify(packageDef, null, 2))
---
+++ src/parsers/fp-ts/schema.ts
@@ -0,0 +1,3 @@
+import * as S from "@fp-ts/schema/Schema";
+
+export const OpenApi = S.struct({});
