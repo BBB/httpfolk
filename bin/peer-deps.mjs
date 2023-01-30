e256758402fc54232b40a5d7aaa8ae387a9fd71d
Ollie Relph <ollie@relph.me>
2023-01-30 20:20:06 +0000

    format

--- bin/peer-deps.mjs
+++ bin/peer-deps.mjs
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 
-import fs from "node:fs"
-import packageDef from "../package.json" assert {type: "json"};
+import fs from "node:fs";
+import packageDef from "../package.json" assert { type: "json" };
 
 const peers = {
-    "@fp-ts/schema": "^0.0.8"
-}
+  "@fp-ts/schema": "^0.0.8",
+};
 
-packageDef.peerDependencies = peers
+packageDef.peerDependencies = peers;
 packageDef.devDependencies = {
-    ...packageDef.devDependencies,
-    ...peers
-}
+  ...packageDef.devDependencies,
+  ...peers,
+};
 
-fs.writeFileSync("package.json", JSON.stringify(packageDef, null, 2))
+fs.writeFileSync("package.json", JSON.stringify(packageDef, null, 2));
