a53f522510dcb13154d7dd01b9df7063854bb818
Ollie Relph <ollie@relph.me>
2023-02-19 11:28:29 +0000

    Add flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "BBB/openapi";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = [
+          pkgs.nodejs-16_x
+          pkgs.nodePackages.pnpm ];
+      };
+    });
+}
