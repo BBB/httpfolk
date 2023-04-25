5ea6244cb64306cf9c342ad98ae6d415082d0d2d
Ollie Relph <ollie@relph.me>
2023-04-25 22:18:31 +0100

    lazy to avoid circular dep

--- src/parsers/effect-schema/CallbackObject.ts
+++ src/parsers/effect-schema/CallbackObject.ts
@@ -3,5 +3,7 @@ import { referenceOr } from "./ReferenceObject";
 
 import { PathItemObject } from "./PathItemObject";
 
-export const CallbackObject = S.record(S.string, referenceOr(PathItemObject));
+export const CallbackObject = S.lazy(() =>
+  S.record(S.string, referenceOr(PathItemObject))
+);
 export type CallbackObject = S.To<typeof CallbackObject>;
