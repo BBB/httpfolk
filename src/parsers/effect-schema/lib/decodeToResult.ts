b3cac60bce2ab9e712885370b7784b8ec4b4d012
Ollie Relph <ollie@relph.me>
2023-04-27 16:55:38 +0100

    use parse

--- src/parsers/effect-schema/lib/decodeToResult.ts
+++ src/parsers/effect-schema/lib/decodeToResult.ts
@@ -1,11 +1,12 @@
-import { decodeEither, Schema } from "@effect/schema/Schema";
+import * as S from "@effect/schema/Schema";
+import { Schema } from "@effect/schema/Schema";
 import { ParseOptions } from "@effect/schema/AST";
 import { ParseError } from "@effect/schema/ParseResult";
 import { Result } from "./Result";
 
 export const decodeToResult = <In, Out>(schema: Schema<In, Out>) => {
-  const decodeSchema = decodeEither(schema);
-  return (input: In, options?: ParseOptions): Result<Out, ParseError> => {
+  const decodeSchema = S.parseEither(schema);
+  return (input: unknown, options?: ParseOptions): Result<Out, ParseError> => {
     return Result.ofEither(decodeSchema(input, options));
   };
 };
